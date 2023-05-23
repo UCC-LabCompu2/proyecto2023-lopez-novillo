6d6c554a9e97826445dd84a34d64038e18ea4ddf
MarcosLopezt <marclopeztapia@gmail.com>
2023-05-23 10:06:23 -0300

    java

---
+++ funciones.js
@@ -0,0 +1,13 @@
+/**
+ * Convierte unidades entre metros, pulgadas, pies y yardas.
+ * @method convertirUnidades
+ * @param {string} id - Id del elemento input del html
+ * @param {number} valor - Contiene el valor del input que ingresa el usuario
+ */
+
+genero(valor){
+    var resp;
+    if(valor=="rock"){
+        
+    }
+}
