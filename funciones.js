5e1684251a2cfd9f052c50ca134fa80760e42824
MarcosLopezt <marclopeztapia@gmail.com>
2023-06-03 12:49:01 -0300

    java1.3

--- funciones.js
+++ funciones.js
@@ -5,3 +5,47 @@
  * @param {number} valor - Contiene el valor del input que ingresa el usuario
  */
 
+validacion = () => {
+    var nombre=document.getElementById("nom").value;
+    var edad=document.getElementById("eda").value;
+    if(nombre === ""){
+        alert("Ingresar el nombre");
+        return false;
+
+    }
+    if(!isNaN(nombre)){
+        alert("El nombre no puede ser numero");
+        return false;
+    }
+
+    if(isNaN(edad)){
+        alert("Ingrese edad en numeros");
+    }
+    if(edad===""){
+        alert ("Ingrese la edad");
+    }
+
+    document.getElementById("nom").value = "";
+    document.getElementById("eda").value = "";
+
+    
+    return true;
+
+}
+
+canciones =() =>{
+    var generoM=document.getElementById("genero").value;
+    var estado=document.getElementById("estado").value;
+    var cambio=document.getElementById("cambio").value;
+
+    console.log(generoM);
+    console.log(estado);
+    console.log(cambio);
+
+    if(generoM==="rock" && estado==="Feliz" && cambio==="Si"){
+        window.open("AcercaDe.html");
+    
+    }
+
+    
+}
