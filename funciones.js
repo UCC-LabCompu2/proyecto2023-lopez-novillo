c3511a44ac35eff9e47ec0c93462b83f8828e4d9
MarcosLopezt <marclopeztapia@gmail.com>
2023-06-05 10:28:10 -0300

    Java1.5

--- funciones.js
+++ funciones.js
@@ -15,21 +15,26 @@ validacion = () => {
     }
     if(!isNaN(nombre)){
         alert("El nombre no puede ser numero");
+        document.getElementById("nom").value = "";
+        return false;
+    }
+
+    if(edad===""){
+        alert ("Ingrese la edad");
         return false;
     }
 
     if(isNaN(edad)){
-        alert("Ingrese edad en numeros");
+        alert("La edad no puede estar en letras");
+        document.getElementById("eda").value = "";
+        return false;
     }
-    if(edad===""){
-        alert ("Ingrese la edad");
-    }
+  
+    return true;
 
     document.getElementById("nom").value = "";
     document.getElementById("eda").value = "";
 
-    
-    return true;
 
 }
 
@@ -42,8 +47,86 @@ canciones =() =>{
     console.log(estado);
     console.log(cambio);
 
+    if (validacion()===false){
+        return;
+    }
+
     if(generoM==="rock" && estado==="Feliz" && cambio==="Si"){
-        window.open("AcercaDe.html");
+        window.open("https://www.youtube.com/watch?v=8AHCfZTRGiI");
+    
+    }
+
+    if(generoM==="rock" && estado==="Feliz" && cambio==="No"){
+        window.open("https://www.youtube.com/watch?v=i6RZY4Ar3fw&list=RDi6RZY4Ar3fw&start_radio=1");
+    
+    }
+
+    if(generoM==="rock" && estado==="Triste" && cambio==="Si"){
+        window.open("https://www.youtube.com/watch?v=-nn1veyAgDQ");
+    
+    }
+
+    if(generoM==="rock" && estado==="Triste" && cambio==="No"){
+        window.open("https://www.youtube.com/watch?v=8AHCfZTRGiI");
+    }
+
+    if(generoM==="rockn" && estado==="Feliz" && cambio==="Si"){
+        window.open("https://www.youtube.com/watch?v=1Zjm0uh8oeA");
+    
+    }
+
+    if(generoM==="rockn" && estado==="Feliz" && cambio==="No"){
+        window.open("https://www.youtube.com/watch?v=u4viftk6vBw");
+    
+    }
+
+    if(generoM==="rockn" && estado==="Triste" && cambio==="Si"){
+        window.open("https://www.youtube.com/watch?v=u4viftk6vBw");
+    
+    }
+
+    if(generoM==="rockn" && estado==="Triste" && cambio==="No"){
+        window.open("https://www.youtube.com/watch?v=1Zjm0uh8oeA");
+    
+    }
+
+    if(generoM==="trap" && estado==="Feliz" && cambio==="Si"){
+        window.open("https://www.youtube.com/watch?v=yzlzqYD3vRA");
+    
+    }
+
+    if(generoM==="trap" && estado==="Feliz" && cambio==="No"){
+        window.open("https://www.youtube.com/watch?v=yYVCf-y2YmE");
+    
+    }
+
+    if(generoM==="trap" && estado==="Triste" && cambio==="Si"){
+        window.open("https://www.youtube.com/watch?v=yYVCf-y2YmE");
+    
+    }
+
+    if(generoM==="trap" && estado==="Triste" && cambio==="No"){
+        window.open("https://www.youtube.com/watch?v=yzlzqYD3vRA");
+    
+    }
+
+    if(generoM==="tecno" && estado==="Feliz" && cambio==="Si"){
+        window.open("https://www.youtube.com/watch?v=8AHCfZTRGiI");
+    
+    }
+
+    if(generoM==="tecno" && estado==="Feliz" && cambio==="No"){
+        window.open("https://www.youtube.com/watch?v=8AHCfZTRGiI");
+    
+    }
+
+    if(generoM==="tecno" && estado==="Triste" && cambio==="Si"){
+        window.open("https://www.youtube.com/watch?v=8AHCfZTRGiI");
+    
+    }
+
+    if(generoM==="tecno" && estado==="Triste" && cambio==="No"){
+        window.open("https://www.youtube.com/watch?v=8AHCfZTRGiI");
     
     }
 
